ce0e8e8af9aae47c6083fc0a35e9134cb157ddbb
Pedro Mantica <pmantica@mit.edu>
2018-12-05 18:03:49 +0000

    First commit in a while

--- code/model.py
+++ code/model.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 from keras_one_cycle.clr import LRFinder, OneCycleLR
-from keras.applications import MobileNet
+from keras.applications import MobileNet, ResNet50
 from keras.callbacks import ModelCheckpoint, ReduceLROnPlateau
 from keras.layers import Dense, Flatten
 from keras.losses import categorical_crossentropy
@@ -20,7 +20,7 @@ from perf_logger import *
 from preprocessing import image_generator_xd, df_to_image_array_xd
 from se_resnext import SEResNextImageNet
 
-EXPERIMENT_NAME = "128x128_mobile_net_super_convergence"
+EXPERIMENT_NAME = "128x128_resnet50"
 ex = Experiment(EXPERIMENT_NAME)
 ex.observers.append(MongoObserver.create())  # hook into the MongoDB
 ex.captured_out_filter = apply_backspaces_and_linefeeds  # make output more readable
@@ -29,18 +29,18 @@ np.random.seed(seed=1987)
 tf.set_random_seed(seed=1987)
 
 
-#@ex.config
+@ex.config
 def track_params():
-    batch_size = 512
-    num_samples = 500
+    batch_size = 128
+    num_samples = 5000000
     steps = num_samples // batch_size
-    epochs = 1
-    size = 128
+    epochs = 40
+    size = 512
     lw = 6
     saved_model = None  # "weights/128x128_mobilenet_copy.hdf5"
 
 
-#@ex.capture
+@ex.capture
 def log_performance(_run, logs):
     _run.add_artifact("weights/" + EXPERIMENT_NAME + "_weights_" + ".hdf5")
     _run.log_scalar("categorical_crossentropy", float(logs.get('categorical_crossentropy')))
@@ -52,7 +52,7 @@ def log_performance(_run, logs):
     _run.result = float(logs.get('val_top_3_accuracy'))
 
 
-#@ex.automain
+@ex.automain
 def main(batch_size, epochs,
          steps, size, lw, saved_model):
     valid_df = pd.read_csv(INPUT_FOLDER + 'train_k{}.csv.gz'.format(NCSVS - 1), nrows=3000)
@@ -61,19 +61,9 @@ def main(batch_size, epochs,
     x_valid = df_to_image_array_xd(valid_df, size, lw)
     y_valid = keras.utils.to_categorical(valid_df.y, num_classes=NCATS)
 
-    # Ensure that number of epochs = 1 when calling fit()
-    # model = MobileNet(weights=None, include_top=False, input_shape=(size, size, 1), alpha=1.)
-    # model.compile(optimizer=Adam(lr=0.002), loss='categorical_crossentropy',
-    #               metrics=[categorical_crossentropy, categorical_accuracy, top_3_accuracy])
-    # x = base_model.output
-    # x = Flatten()(x)
-    # predictions = Dense(NCATS, activation='softmax')(x)
-    # model = Model(inputs=base_model.input, outputs=predictions)
-    # model = base_model
-    model = MobileNet(input_shape=(size, size, 1), alpha=1., weights=None, classes=NCATS)
-    model.compile(optimizer=Adam(lr=0.000125), loss='categorical_crossentropy',
+    model = ResNet50(weights=None, input_shape=(size, size, 1), classes=340)
+    model.compile(optimizer=Adam(lr=0.0002), loss='categorical_crossentropy',
                   metrics=[categorical_crossentropy, categorical_accuracy, top_3_accuracy])
-
     print("finished compiling model")
     if saved_model is not None:
         model.load_weights(saved_model)
@@ -81,11 +71,13 @@ def main(batch_size, epochs,
     model.fit_generator(
         train_datagen, steps_per_epoch=steps, epochs=epochs, verbose=1,
         validation_data=(x_valid, y_valid),
-        callbacks=[ModelCheckpoint("weights/" + EXPERIMENT_NAME + "_weights_" + ".hdf5", monitor='val_loss',
-                                   save_best_only=True, mode='auto', period=1),
-                   OneCycleLR(num_samples=batch_size*steps, num_epochs=epochs, batch_size=batch_size, max_lr=0.006,
-                              maximum_momentum=None, minimum_momentum=None),
-                   ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001)
+        callbacks=[
+                   ModelCheckpoint("weights/" + EXPERIMENT_NAME + "_weights_" + ".hdf5", monitor='val_loss',
+                                           save_best_only=True, mode='auto', period=1),
+                   LogPerformance(log_performance),
+                   ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=5, min_lr=0.001),
+                   # LRFinder(batch_size * steps, batch_size, minimum_lr=1e-5, maximum_lr=1, lr_scale='exp',
+                   #          save_dir="learning_rate_losses/", verbose=True)
                    ]
     )
 
@@ -93,14 +85,14 @@ def main(batch_size, epochs,
     print('Test loss:', score[0])
     print('Test accuracy:', score[1])
     return score[1]
-
-
-if __name__ == "__main__":
-    batch_size = 512
-    num_samples = 5000000
-    steps = num_samples // batch_size
-    epochs = 40
-    size = 128
-    lw = 6
-    saved_model = None  # "weights/128x128_mobilenet_copy.hdf5"
-    main(batch_size=batch_size, steps=steps, epochs=epochs, lw=lw, size=size, saved_model=saved_model)
+#
+#
+# if __name__ == "__main__":
+#     batch_size = 512
+#     num_samples = 5000000
+#     steps = num_samples // batch_size
+#     epochs = 40
+#     size = 128
+#     lw = 6
+#     saved_model = None  # "weights/128x128_mobilenet_copy.hdf5"
+#     main(batch_size=batch_size, steps=steps, epochs=epochs, lw=lw, size=size, saved_model=saved_model)
--- code/utils/perf_logger.py
+++
@@ -1,9 +0,0 @@
-from keras.callbacks import Callback
-
-class LogPerformance(Callback):
-    def __init__(self, log_func):
-        Callback.__init__(self)
-        self.log_func = log_func
-
-    def on_epoch_end(self, _, logs={}):
-        self.log_func(logs=logs)
